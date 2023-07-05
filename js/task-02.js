2135169584880b7c207b8e1c7e5a2db4e459cb7d
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-05 21:17:22 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -30,7 +30,7 @@ const listEl = document.querySelector("#ingredients");
 // listEl.append(itemEl1, itemEl2, itemEl3, itemEl4, itemEl5, itemEl6);
 
 const itemsEl = [];
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < ingredients.length; i++) {
   const item = document.createElement("li");
   item.textContent = ingredients[i];
   item.classList.add("item");
